fe5574e63b59005e4a20df73b4305811dbef0880
King-Hard <208336213+King-Hard@users.noreply.github.com>
2025-06-20 19:34:44 +0800

    .

--- src/Shared/Partial/Header.jsx
+++ src/Shared/Partial/Header.jsx
@@ -1,32 +1,16 @@
 import {Link} from 'react-router-dom'
-import HeaderItem from './HeaderItem'
-import MainLogo from '../../assets/Images/MainLogo.png'
-import ironman from '../../assets/Images/ironman.jpg'
-import {HiHome, HiMagnifyingGlass, HiStar, HiPlayCircle, HiTv} from 'react-icons/hi2'
-import {HiPlus, HiDotsVertical} from 'react-icons/hi'
 
 const Header = () => {
-  const menu = [
-    {name: "Home", icon: HiHome},
-    {name: "Search", icon: HiMagnifyingGlass},
-    {name: "Watch List", icon: HiPlus},
-    {name: "Originals", icon: HiStar},
-    {name: "Movies", icon: HiPlayCircle},
-    {name: "Series", icon: HiTv}
-  ]
 
   return (
-    <div className='flex border justify-between rounded rounded-b-lg p-5 text-lg text-center'>
+    <div className='flex justify-between border rounded rounded-b-lg p-5'>
       <div>
-        <img src={MainLogo} className='w-[80px] md:w-[115px] object-cover'/>
+        <h1>Header</h1>
       </div>
-      <div className='flex gap-7'>
-        {menu.map((item, index) =>(
-          <HeaderItem key={index} name={item.name} Icon={item.icon} />
-        ))}
-      </div >
-      <div>
-        <img src={ironman} className='object-cover w-[40px] rounded-full'/>
+      <div className='flex gap-8'>
+        <Link to='/'>Home</Link>
+        <Link to='/About'>About</Link>
+        <Link to='/Contact'>Contact</Link>
       </div>
     </div>
   )
